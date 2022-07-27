f5b38039282718559b2cfbba6478623d528e2821
FabricioGurgel <fabriciogurgel.85@gmail.com>
2022-07-26 22:29:01 -0300

    projetos de tipo primitivo

---
+++ AtividadeSobreVariavel/src/atividade01/Atividade.java
@@ -0,0 +1,16 @@
+package atividade01;
+
+public class Atividade {
+
+	public static void main(String[] args) {
+		boolean valorLojico = true;
+		int valorInteiro = 2;
+		String nomeProfessor = "Lucas";
+		double valorReal = 23.5;
+		
+		//atividade de casa declaras 10 variaves de cada tipo alternando os valores atribuido 
+		//fazer um augoritimo que calcule o valor de uma variavel do tipo int elevado ao quadrado
+		//leia sobre if, if else, else
+	}
+
+}
