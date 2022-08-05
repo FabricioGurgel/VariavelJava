12a9f7207e1d46857bf72b8629346c9d5bcf35bf
FabricioGurgel <fabriciogurgel.85@gmail.com>
2022-08-04 21:43:57 -0300

    calculo da aerea do cubo

---
+++ AtividadeSobreVariavel/src/atividade01/TesteDeComhecimento1.java
@@ -0,0 +1,13 @@
+package atividade01;
+
+public class TesteDeComhecimento1 {
+
+	public static void main(String[] args) {
+		int medidaParede = 3;
+		int areaBase;
+		
+		areaBase = 6*(medidaParede*medidaParede);
+		System.out.println(areaBase);
+	}
+
+}
